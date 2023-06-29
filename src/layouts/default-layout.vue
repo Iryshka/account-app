6ad557ae8a6ab9606fd43e8b2d21b8f355bcb938
Iryshka Ka <iryna292@gmail.com>
2023-06-29 19:08:29 +0300

    Updated files

---
+++ src/layouts/default-layout.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="default-layout">
+    <header>
+      <default-header />
+    </header>
+    <main></main>
+    <footer>
+      <default-footer />
+    </footer>
+  </div>
+</template>
+<script setup>
+import DefaultHeader from '../components/layouts/default/DefaultHeader.vue'
+import DefaultFooter from '../components/layouts/default/DefaultFooter.vue'
+</script>
